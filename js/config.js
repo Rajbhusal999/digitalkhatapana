ac4f7591527f47b08ce106d951aedf6eaf7bbf99
Your Name <you@example.com>
2026-06-03 15:15:04 +0545

    Update Supabase URL

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@
  * Project Settings -> API -> Project API Keys
  */
 
-const SUPABASE_URL = "https://YOUR_SUPABASE_PROJECT_URL.supabase.co";
+const SUPABASE_URL = "https://gxmmjkyakfwkhqtyizdk.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_0Ucsj5RFIXxxYRRE3DmatQ_Q0PT2Zwx";
